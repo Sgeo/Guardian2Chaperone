1c52457f82005c2c1bcd7de9c66fbb0151caf179
Sgeo <sgeoster@gmail.com>
2017-03-20 22:14:52 -0400

    Remove unhelpful exit(0), and remove hopefully inaccurate license header.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,4 @@
-/************************************************************************************
-Authors     :   Bruno Evangelista
-Copyright   :   Copyright 2016 Oculus VR, LLC All Rights reserved.
 
-Licensed under the Oculus VR Rift SDK License Version 3.3 (the "License"); 
-you may not use the Oculus VR Rift SDK except in compliance with the License, 
-which is provided at the time of installation or download, or which 
-otherwise accompanies this software in either electronic or hard copy form.
-
-You may obtain a copy of the License at
-http://www.oculusvr.com/licenses/LICENSE-3.3 
-
-Unless required by applicable law or agreed to in writing, the Oculus VR SDK 
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*************************************************************************************/
 
 #pragma warning(disable: 4324)
 #include "OVR_CAPI_D3D.h" // Oculus SDK
@@ -168,7 +151,6 @@ void GuardianSystemDemo::Start(HINSTANCE hinst)
 
 	ovr_Shutdown();
 
-	exit(0);
 
     
 
